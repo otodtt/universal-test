3cedea86323e184e647aca041e7174ac321f6fc3
otodtt <dtenevoto@mail.bg>
2020-11-19 14:14:26 +0200

    add env

---
+++ src/environments/firebase.ts
@@ -0,0 +1,10 @@
+export const firebase = {
+    firebase: {
+        apiKey: 'AIzaSyBm7KBzmZ_1lYNKSf0uEGD1WXyLhhKGVHw',
+        authDomain: 'prz-test.firebaseapp.com',
+        databaseURL: 'https://prz-test.firebaseio.com',
+        projectId: 'prz-test',
+        storageBucket: 'prz-test.appspot.com',
+        messagingSenderId: '1000711935459'
+      }
+};
